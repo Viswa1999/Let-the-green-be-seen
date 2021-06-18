581b7f4099ee35bda1bd916b7c410aff68903036
Viswa1999 <85360991+Viswa1999@users.noreply.github.com>
2021-06-18 12:28:26 +0530

    Delete form.py

--- image/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-#from django.forms import fields # in-built django forms
-from .models import Image
-
-class ImageForm(forms.ModelForm):
-    class Meta: 
-        model = Image
-        fields=("caption","image")
